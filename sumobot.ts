6acb8e37503ff3972b6e89281c0b26d6eb5e4458
johnphua <phuajohn0@gmail.com>
2017-10-18 23:59:43 +0800

    Added faceEnemy after search

--- sumobot.ts
+++ sumobot.ts
@@ -8,6 +8,7 @@ var motorEye = ev3.motorC();
 var colorSensor = ev3.colorSensor();
 var ultrasonic = ev3.ultrasonicSensor();
 var touchSensor = ev3.touchSensor2();
+var gyroSensor = ev3.gyroSensor();
 
 if (ev3.connected(motorA)) {
     source.alert("MOTOR A CONNECTED");
@@ -29,38 +30,99 @@ if (ev3.connected(ultrasonic)) {
     source.alert("ULTRASONIC SENSOR CONNECTED");
 }
 
+if (ev3.connected(touchSensor)) {
+    source.alert("TOUCH SENSOR CONNECTED");
+}
+
+if (ev3.connected(gyroSensor)) {
+    source.alert("GYRO SENSOR CONNECTED");
+}
+
 var calib_rotate_distance = 95;
 var black_val = 10;
 
-var front_position = ev3.motorGetPosition(motorEye);
-source.alert(front_position);
-
-var view_angle = 60;
-var left_angle = front_position - view_angle;
-var right_angle = front_position + view_angle;
-
 var target_distance = 40;
+var view_angle = 60;
+
+function turnClockwise() {
+    ev3.runForTime(motorA, 1000, -200);
+    ev3.runForTime(motorB, 1000, 200);
+}
+
+function turnAntiClockwise() {
+    ev3.runForTime(motorA, 1000, 200);
+    ev3.runForTime(motorB, 1000, -200);
+}
+
+function faceEnemy(bearing) {
+    ev3.gyroSensorRateMode(gyroSensor);
+    ev3.gyroSensorAngleMode(gyroSensor);
+    function leftBearingHit() {
+        return ev3.gyroSensorAngle(gyroSensor) <= bearing;
+    }
+    function rightBearingHit() {
+        return ev3.gyroSensorAngle(gyroSensor) >= bearing;
+    }
+    if(bearing !== 0 && bearing < 0) {
+        ev3.runUntil(leftBearingHit, turnAntiClockwise);
+    } else {
+        ev3.runUntil(rightBearingHit, turnClockwise);
+    }
+}
 
 function search() {
+    var front_position = ev3.motorGetPosition(motorEye);
+    source.alert("Front postion" + front_position);
+    var left_angle = front_position - view_angle;
+    var right_angle = front_position + view_angle;
+
    var current_angle = ev3.motorGetPosition(motorEye);
    while(ev3.ultrasonicSensorDistance(ultrasonic) > target_distance && current_angle > left_angle) {
      ev3.runToAbsolutePosition(motorEye, left_angle, 200);
      current_angle = ev3.motorGetPosition(motorEye);
-   } 
+   }
    while(ev3.ultrasonicSensorDistance(ultrasonic) > target_distance && current_angle < right_angle) {
      ev3.runToAbsolutePosition(motorEye, right_angle, 200);
      current_angle = ev3.motorGetPosition(motorEye);
    }
    var object_distance = ev3.ultrasonicSensorDistance(ultrasonic);
    ev3.motorStop(motorEye);
-   return object_distance <= target_distance ? object_distance : search(); 
+   ev3.runToAbsolutePosition(motorEye, front_position, 200);
+   var enemy_bearing = current_angle - front_position;
+   if(object_distance <= target_distance) {
+     faceEnemy(enemy_bearing);
+     return source.pair(1, 1);
+   } else {
+     search();
+ }
 }
 
 function assault(object_distance) {
     while (ev3.ultrasonicSensorDistance(ultrasonic) <= object_distance) {
-      ev3.runForTime(motorA, 1000, 200);
-      ev3.runForTime(motorB, 1000, 200);
-    }   
+      ev3.runForTime(motorA, 5000, 700);
+      ev3.runForTime(motorB, 5000, 700);
+    }
+    search();
+}
+
+function check_bumper() {
+  return (ev3.touchSensorPressed(touchSensor)) ? true : check_bumper();
+}
+
+function move_forward(distance) {
+    ev3.runForDistance(motorA, distance, 700);
+    ev3.runForDistance(motorB, distance, 700);
+}
+
+function defend() {
+  function turn_180() {
+    // to calibrate
+    ev3.runForDistance(motorA, -170, 500);
+    ev3.runForDistance(motorB, 170, 500);
+    ev3.pause(2000);
+  }
+  turn_180();
+  move_forward(50);
 }
 
 while(true) {
